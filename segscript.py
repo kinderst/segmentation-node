07fb64af37f4e73fbfdc10773304890a98b960bc
kinderst <kinders@uw.edu>
2023-04-26 14:49:34 -0600

    trying

--- segscript.py
+++ segscript.py
@@ -33,96 +33,10 @@ from tensorflow.keras import layers
 from gcloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-def convolution_block(
-        block_input,
-        num_filters=256,
-        kernel_size=3,
-        dilation_rate=1,
-        padding="same",
-        use_bias=False,
-):
-    x = layers.Conv2D(
-        num_filters,
-        kernel_size=kernel_size,
-        dilation_rate=dilation_rate,
-        padding="same",
-        use_bias=use_bias,
-        kernel_initializer=keras.initializers.HeNormal(),
-    )(block_input)
-    x = layers.BatchNormalization()(x)
-    return tf.nn.relu(x)
-
-
-def DilatedSpatialPyramidPooling(dspp_input):
-    dims = dspp_input.shape
-    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
-    x = convolution_block(x, kernel_size=1, use_bias=True)
-    out_pool = layers.UpSampling2D(
-        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]), interpolation="bilinear",
-    )(x)
-
-    out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
-    out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
-    out_12 = convolution_block(dspp_input, kernel_size=3, dilation_rate=12)
-    out_18 = convolution_block(dspp_input, kernel_size=3, dilation_rate=18)
-
-    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
-    output = convolution_block(x, kernel_size=1)
-    return output
-
-
-def DeeplabV3Plus(image_size, num_classes):
-    model_input = keras.Input(shape=(image_size, image_size, 3))
-    resnet50 = keras.applications.ResNet50(
-        weights="imagenet", include_top=False, input_tensor=model_input
-    )
-    x = resnet50.get_layer("conv4_block6_2_relu").output
-    x = DilatedSpatialPyramidPooling(x)
-
-    input_a = layers.UpSampling2D(
-        size=(image_size // 4 // x.shape[1], image_size // 4 // x.shape[2]),
-        interpolation="bilinear",
-    )(x)
-    input_b = resnet50.get_layer("conv2_block3_2_relu").output
-    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
-
-    x = layers.Concatenate(axis=-1)([input_a, input_b])
-    x = convolution_block(x)
-    x = convolution_block(x)
-    x = layers.UpSampling2D(
-        size=(image_size // x.shape[1], image_size // x.shape[2]),
-        interpolation="bilinear",
-    )(x)
-    model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same")(x)
-    return keras.Model(inputs=model_input, outputs=model_output)
-
-
-def read_single_img(img_param):
-    image = tf.convert_to_tensor(img_param)
-    image.set_shape([None, None, 3])
-    image = tf.image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
-    image = tf.keras.applications.resnet50.preprocess_input(image)
-    return image
-
-def infer(model, image_tensor):
-    predictions = model.predict(np.expand_dims((image_tensor), axis=0))
-    predictions = np.squeeze(predictions)
-    predictions = np.argmax(predictions, axis=2)
-    return predictions
-
-
-def decode_segmentation_masks(mask, colormap_param, n_classes):
-    r = np.zeros_like(mask).astype(np.uint8)
-    g = np.zeros_like(mask).astype(np.uint8)
-    b = np.zeros_like(mask).astype(np.uint8)
-    for l in range(0, n_classes):
-        idx = mask == l
-        r[idx] = colormap_param[l, 0]
-        g[idx] = colormap_param[l, 1]
-        b[idx] = colormap_param[l, 2]
-    rgb = np.stack([r, g, b], axis=2)
-    return rgb
+from deeplabutils import DeeplabV3Plus
+from deeplabutils import read_single_img
+from deeplabutils import infer
+from deeplabutils import decode_segmentation_masks
 
 
 credentials_dict = {
@@ -195,7 +109,7 @@ model.load_weights('./deeplabv3weights.h5')
 # num_objects = len(np.unique(mask)) - 1
 
 url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"  # Replace with the actual URL of the video
-filename = "theinput.mp4"  # Replace with the desired name of the video file
+filename = "theinputagain.mp4"  # Replace with the desired name of the video file
 
 response = requests.get(url)
 if response.status_code == 200:
@@ -295,8 +209,13 @@ with torch.cuda.amp.autocast(enabled=True):
 out.release()
 cv2.destroyAllWindows()
 
+print('writing to bucket')
 blob = bucket.blob(output_file_name)
 blob.upload_from_filename(output_file_name)
+
+print('deleting from local storage')
+os.remove(input_file_name)
+os.remove(output_file_name)
 
 # num_objects = None
 # mask = None
